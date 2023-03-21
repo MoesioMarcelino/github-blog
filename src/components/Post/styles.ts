aa331153528ef9c8eef2bdfe7482dc97c20440c8
Moesio Marcelino <moesiomarcelino1@gmail.com>
2023-03-21 19:29:06 -0300

    feat: post component

---
+++ src/components/Post/styles.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 32px;
+  background-color: ${(props) => props.theme['base-post']};
+  border: 2px solid ${(props) => props.theme['base-post']};
+  cursor: pointer;
+  border-radius: 10px;
+
+  > header {
+    display: flex;
+    align-items: baseline;
+
+    h2 {
+      color: ${(props) => props.theme['base-title']};
+    }
+
+    span {
+      white-space: nowrap;
+      color: ${(props) => props.theme['base-span']};
+    }
+  }
+
+  p {
+    color: ${(props) => props.theme['base-text']};
+
+    display: -webkit-box;
+    -webkit-line-clamp: 4;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  &:hover {
+    border: 2px solid ${(props) => props.theme['base-label']};
+    transition: border 0.2s;
+  }
+`
