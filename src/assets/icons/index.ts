fb27e7efb700ccf3806c1a95a4b71d91929886c3
Moesio Marcelino <moesiomarcelino1@gmail.com>
2023-03-23 17:00:49 -0300

    feat: ddd icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,5 +3,16 @@ import { ArrowUpRightIcon } from './arrow-up-right'
 import { BuildingIcon } from './building'
 import { GithubIcon } from './github'
 import { UserGroupIcon } from './user-group'
+import { ArrowLeftIcon } from './arrow-left'
+import { CalendarIcon } from './calendar'
+import { CommentIcon } from './comment'
 
-export { ArrowUpRightIcon, BuildingIcon, GithubIcon, UserGroupIcon }
+export {
+  ArrowUpRightIcon,
+  BuildingIcon,
+  GithubIcon,
+  UserGroupIcon,
+  ArrowLeftIcon,
+  CalendarIcon,
+  CommentIcon,
+}
